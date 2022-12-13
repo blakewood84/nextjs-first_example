7304c6d6ac61a9b01115393c66b80dd2a176c2a8
Blake Wood Jr <blakewoodjr84@gmail.com>
2022-12-13 15:41:22 -0800

    added image. added head

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>My Amazing Title</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
